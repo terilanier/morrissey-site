6490173ac722293bf71ddfdc53db006be18d30db
Teri Lanier <teri.lanier@gmail.com>
2018-11-03 18:59:43 -0700

    adding icons and lightbox fix
    arrgh to W3Schools modal lightbox. throwing javascript error

--- assets/javascripts/jslightbox.js
+++ assets/javascripts/jslightbox.js
@@ -1,7 +1,9 @@
+// Open the Modal
 function openModal() {
   document.getElementById('myModal').style.display = "block";
 }
 
+// Close the Modal
 function closeModal() {
   document.getElementById('myModal').style.display = "none";
 }
@@ -9,10 +11,12 @@ function closeModal() {
 var slideIndex = 1;
 showSlides(slideIndex);
 
+// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
+// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -21,16 +25,16 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("demo");
-  // var captionText = document.getElementById("caption");
+  var captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace("active", "");
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  dots[slideIndex-1].className += "active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
